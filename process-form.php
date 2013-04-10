bf3529746f7d6ee02e97037d9840ea203b692966
Kathy Reid <kathy@kathyreid.id.au>
2013-04-11 05:28:05 +1000

    fixed up some form processing stuff

--- process-form.php
+++ process-form.php
@@ -1,15 +1,13 @@
 <?php
+$mail_body = null;
 while (list($key, $value) = each($_POST)) {
     $mail_body .= $key . ' => ' . $value . "\n";
 }
-
 $Name = "vlined site"; //senders name
 $email = "info@vlined.net"; //senders e-mail adress
-$recipient = "kathy@kathyreid.id.au"; //recipient
-$mail_body = "The text for the mail..."; //mail body
+$recipient = "excuse@vlined.net"; //recipient
 $subject = "Excuse"; //subject
 $header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields
-
 mail($recipient, $subject, $mail_body, $header); //mail command :)
 header("Location: suggestion-thanks.html");
 ?>
