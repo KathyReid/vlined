e669ce254389409b9dcddfc90cb8df06fffa4ac7
KathyReid <kathy@kathyreid.id.au>
2015-07-25 10:45:58 +1000

    changed all the vlined stuff to lca2016 stuff. Many commit.

--- xmllist.js
+++ xmllist.js
@@ -1,84 +1,81 @@
-excusesList = {
-	  
+reasonsList = {
+
 	    //general settings
-	    xml: "excuses.xml",
+	    xml: "reasons.xml",
      display: '1', //number of items to be displayed
 	    random: true, //display randomly {true|false}
-     appendTo: '#excuse', //set the id/class to insert XML data
-     
+     appendTo: '#reason', //set the id/class to insert XML data
+
     init: function () {
-     
+
         //jQuery ajax call to retrieve the XML file
 	        $.ajax({
 	            type: "GET",
-	            url: "excuses.xml",
-	            dataType: "xml",          
-	            success:excusesList.parseXML
+	            url: "reasons.xml",
+	            dataType: "xml",
+	            success:reasonsList.parseXML
         });
-	      
+
 	    }, // end: init()
-      
+
 	    parseXML: function (xml) {
-	      
+
 	        //Grab every single excuse tags in the XML file
-	        var data = $('excuse', xml).get();
+	        var data = $('reason', xml).get();
 
 	        //Allow user to toggle display randomly or vice versa
-	        var list = (excusesList.random) ? excusesList.randomize(data) : data;
+	        var list = (reasonsList.random) ? reasonsList.randomize(data) : data;
 	        var i = 1;
 
 
         //Loop through all the excuses
 	        $(list).each(function () {
-	              
+
 
 	            //Parse data and embed it with HTML
-	            excusesList.insertHTML($(this));    
-      
-	  
+	            reasonsList.insertHTML($(this));
+
+
 	            //If it reached user predefined total of display item, stop the loop, job done.
-            if (i == excusesList.display) return false;
+            if (i == reasonsList.display) return false;
 	            i++;
 	        });
-  
-	      
+
+
 	    }, // end: parseXML()
-  
-    insertHTML: function (excuse) {
-	  
+
+    insertHTML: function (reason) {
+
 	        //retrieve each of the data field from excuse
-        var destination = excuse.find('destination').text();
-        var line = excuse.find('line').text();
-	        var status = excuse.find('status').text();
-	        var text = excuse.find('text').text();
+        var text = reason.find('text').text();
         var html;
-        
+
 
 
         //Embed them into HTML code
-        html = '<div class="excuse">';
+        html = '<div class="reason">';
         html +='<h3>' ;
-	        html += 'Your V/line service to ' + destination +  ' is ' + status  + ' ' +  text;
+	        html += text;
 	        html += '</h3>';
         html += '</div>';
-	          
+
 	        //Append it to user predefined element
-	        $(html).appendTo(excusesList.appendTo);
-	          
+	        $(html).appendTo(reasonsList.appendTo);
+
 	    }, // end: insertHTML()
-	  
-     
+
+
 	    randomize: function(arr) {
-	          
+
 	        //randomize the data
 	        //Credit to JSFromHell http://jsfromhell.com/array/shuffle
         for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
 	            return arr;
-	    
-	    } // end: randomize()  
-	      
-	  
+
+	    } // end: randomize()
+
+
 	}
-	  
+
 //Run this script
-excusesList.init();
+reasonsList.init();
